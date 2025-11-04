73eb3f17e758341cf7eecb3975e62e87ba24c25d
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-04 18:05:44 -0300

    educationModel concluido

---
+++ curriculo/api/models/educationModel.js
@@ -0,0 +1,12 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  const Education = sequelize.define("Education", {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    institution: { type: DataTypes.STRING, allowNull: false },
+    course: { type: DataTypes.STRING, allowNull: false },
+    startDate: { type: DataTypes.DATEONLY },
+    endDate: { type: DataTypes.DATEONLY },
+  });
+  return Education;
+};
