e090796ef451b8cd7ab75872bed45ab9556b17ee
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-04 18:02:15 -0300

    experienceModel concluido

---
+++ curriculo/api/models/experienceModel.js
@@ -0,0 +1,13 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  const Experience = sequelize.define("Experience", {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    company: { type: DataTypes.STRING, allowNull: false },
+    position: { type: DataTypes.STRING, allowNull: false },
+    startDate: { type: DataTypes.DATEONLY },
+    endDate: { type: DataTypes.DATEONLY },
+    description: { type: DataTypes.TEXT },
+  });
+  return Experience;
+};
