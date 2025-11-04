e8a095107060ffd795d86ddc5d5992d026b41dd5
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-04 18:38:04 -0300

    experienceController.js concluido

---
+++ curriculo/api/controllers/experienceController.js
@@ -0,0 +1,78 @@
+import { Experience, User } from "../models/index.js";
+
+export const getAllExperiences = async (req, res) => {
+  try {
+    const experiences = await Experience.findAll({ include: User });
+    res.json(experiences);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getExperienceById = async (req, res) => {
+  try {
+    const experience = await Experience.findByPk(req.params.id, {
+      include: User,
+    });
+    if (!experience)
+      return res.status(404).json({ error: "Experiência não encontrada" });
+    res.json(experience);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createExperience = async (req, res) => {
+  try {
+    const { company, position, startDate, endDate, description, userId } =
+      req.body;
+    const experience = await Experience.create({
+      company,
+      position,
+      startDate,
+      endDate,
+      description,
+      userId,
+    });
+    res.status(201).json(experience);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateExperience = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const experience = await Experience.findByPk(id);
+    if (!experience)
+      return res.status(404).json({ error: "Experiência não encontrada" });
+
+    const { company, position, startDate, endDate, description, userId } =
+      req.body;
+    await experience.update({
+      company,
+      position,
+      startDate,
+      endDate,
+      description,
+      userId,
+    });
+    res.json(experience);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteExperience = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const experience = await Experience.findByPk(id);
+    if (!experience)
+      return res.status(404).json({ error: "Experiência não encontrada" });
+
+    await experience.destroy();
+    res.json({ message: "Experiência removida com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
