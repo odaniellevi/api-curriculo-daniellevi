2a149c778f9f09521a8119bdc6fd45d478508d0e
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-06 14:27:55 -0300

    correção de models/index.js

--- curriculo/api/models/index.js
+++ curriculo/api/models/index.js
@@ -9,6 +9,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const User = UserModel(sequelize);
